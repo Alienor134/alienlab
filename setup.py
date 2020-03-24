c6b83a7a5574f0ec54a83f54d6be57f0df47098f
Alienor34 <alienor.mz@gmail.com>
2020-03-24 11:13:23 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="alienlab", # Replace with your own username
+    version="0.0.1",
+    author="Alienor Lahlou",
+    author_email="alienor.lahlou@ens.fr",
+    description="Adaptation of matplotlib",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    url="https://github.com/Alienor134/alienlab",
+    packages=setuptools.find_packages(),
+    #classifiers=[
+    #    "Programming Language :: Python :: 3",
+    #    "License :: OSI Approved :: MIT License",
+    #   "Operating System :: OS Independent",
+    #],
+    python_requires='>=3.6',
+)
